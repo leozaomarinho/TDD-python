58fa30be0ac5c45e43de6d02416b1478fd33abe3
leonardo <leowalkg6@Gmail.com>
2025-03-09 19:57:32 -0300

    criando services

---
+++ src/services.py
@@ -0,0 +1,10 @@
+class BancoDeDados
+    def obter_saldo(self,titular):
+        raise NotImplementedError("Deve ser implementado!")
+
+class ServicoConta:
+    def __init__(self,banco:BancoDeDados):
+        self.banco = banco
+
+    def verificar_saldo(self,titular):
+        return self.banco.obter_saldo(titular)
