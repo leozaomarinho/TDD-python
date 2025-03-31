4d7b031e681706240dcadfc7a237b014679cc734
leonardo <leowalkg6@Gmail.com>
2025-03-31 18:04:33 -0300

    build models class

--- src/models.py
+++ src/models.py
@@ -24,3 +24,26 @@ class ContaBancaria:
         conta_destino.saldo += valor
         self.historico.append(f"Transferencia de -{valor} para {conta_destino.titular}")
         conta_destino.historico.append(f"Transferencia de +{valor} de {self.titular}")
+
+class Banco:
+    def __init__(self):
+        self.contas = {}
+
+    def adicionar_conta(self,titular:str,saldo =0):
+        if titular in self.contas:
+            raise ValueError("Conta já existe.")
+        self.contas[titular] = ContaBancaria(titular,saldo)
+    
+    def buscar_conta(self,titular:str):
+        if titular not in self.contas:
+            raise ValueError("Conta não encontrada.")
+        return self.contas[titular]
+
+    def transferir(self,titular_origem:str,titular_destino:str,valor:float):
+        
+        if titular_origem not in self.contas or titular_destino not in self.contas:
+            raise ValueError("Conta não encontrada.")
+        
+        conta_origem = self.buscar_conta(titular_origem)
+        conta_destino = self.buscar_conta(titular_destino)
+        conta_origem.transferir(conta_destino,valor)
